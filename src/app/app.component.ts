2ade62b452ad91b32f3d2d341d210bba0d475ec7
Saket Gaonkar <saket2498@gmail.com>
2020-12-24 22:27:26 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task1-youtube';
+  youtubeId = '';
+  videoURL = '';
+  // videoURL = `https://www.youtube.com/embed/${this.youtubeId}`;
+  safeURL: any;
+  e: any;
+  constructor(private sanitizer: DomSanitizer) {
+    console.log(this.youtubeId)
+    // this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
+  }
+  search(item) {
+    console.log(item)
+    this.youtubeId = item;
+    this.videoURL = `https://www.youtube.com/embed/${this.youtubeId}`;
+    this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
+    // console.log(this.youtubeId)
+    // console.log(this.videoURL)
+  }
+  // P1KZHQDEpWo
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
