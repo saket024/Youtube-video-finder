05a809fb3340f8f40dd94b826906eb4712c49b26
Saket Gaonkar <saket2498@gmail.com>
2020-12-27 22:55:33 +0530

    Sanitizing the HTML file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser'
+import { DomSanitizer, SafeUrl, SafeHtml } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
@@ -7,21 +7,44 @@ import { DomSanitizer } from '@angular/platform-browser'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  massTimingsHtml: any;
   title = 'task1-youtube';
-  youtubeId = '';
+  youtubeId: any;
   videoURL = '';
+  item = '';
   // videoURL = `https://www.youtube.com/embed/${this.youtubeId}`;
-  safeURL: any;
+  safeURL: SafeUrl;
+  private _htmlProperty: string = '<input type="text" placeholder="Type video-id here" id="input" />';
   e: any;
+
   constructor(private sanitizer: DomSanitizer) {
-    console.log(this.youtubeId)
+    // this.sanitizer.bypassSecurityTrustUrl(this.youtubeId)
+    // console.log("kjsdniulsduwohd");
+    console.log(this.youtubeId);
     // this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
   }
-  search(item) {
-    console.log(item)
-    this.youtubeId = item;
-    this.videoURL = `https://www.youtube.com/embed/${this.youtubeId}`;
+
+  public get htmlProperty(): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(this._htmlProperty);
+  }
+
+
+  // assembleHtml() {
+  //   var strHTML = '<iframe width="460" height="315" [src]=\'safeURL\' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>'
+  //   this.safeHTML = this.sanitizer.bypassSecurityTrustHtml(strHTML)
+  //   console.log(this.safeHTML);
+  //   return this.safeHTML;
+  // }
+  search() {
+    this.item = (<HTMLInputElement>document.getElementById("input")).value
+    console.log(this.item)
+    this.youtubeId = this.item;
+    this.videoURL = 'https://www.youtube.com/embed/' + 'this.youtubeId';
     this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
+    console.log("safeurl")
+    // this.safeHTML = this.sanitizer.bypassSecurityTrustHtml(`<iframe width="460" height="315" [src]='${this.safeURL}' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>`)
+    // console.log(this.safeHTML)
+    // this.postHTML = this.assembleHtml()
     // console.log(this.youtubeId)
     // console.log(this.videoURL)
   }
