7d4f37d53905c3aaad239415a9fc0ef68f40f559
Saket Gaonkar <saket2498@gmail.com>
2020-12-27 23:36:01 +0530

    html sanitization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,34 +14,30 @@ export class AppComponent {
   item = '';
   // videoURL = `https://www.youtube.com/embed/${this.youtubeId}`;
   safeURL: SafeUrl;
-  private _htmlProperty: string = '<input type="text" placeholder="Type video-id here" id="input" />';
+  private _inputItem: string = '<input type="text" placeholder="Type video-id here" id="input" />';
   e: any;
 
   constructor(private sanitizer: DomSanitizer) {
     // this.sanitizer.bypassSecurityTrustUrl(this.youtubeId)
-    // console.log("kjsdniulsduwohd");
-    console.log(this.youtubeId);
+    // console.log(this.youtubeId);
     // this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
   }
 
-  public get htmlProperty(): SafeHtml {
-    return this.sanitizer.bypassSecurityTrustHtml(this._htmlProperty);
+  public get inputItem(): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(this._inputItem);
   }
 
 
-  // assembleHtml() {
-  //   var strHTML = '<iframe width="460" height="315" [src]=\'safeURL\' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>'
-  //   this.safeHTML = this.sanitizer.bypassSecurityTrustHtml(strHTML)
-  //   console.log(this.safeHTML);
-  //   return this.safeHTML;
-  // }
+
   search() {
     this.item = (<HTMLInputElement>document.getElementById("input")).value
-    console.log(this.item)
+    // console.log(this.item)
     this.youtubeId = this.item;
-    this.videoURL = 'https://www.youtube.com/embed/' + 'this.youtubeId';
+    // console.log("****" + this.youtubeId)
+    this.videoURL = 'https://www.youtube.com/embed/' + this.youtubeId;
+    // console.log(this.videoURL)
     this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
-    console.log("safeurl")
+    // console.log("safeurl")
     // this.safeHTML = this.sanitizer.bypassSecurityTrustHtml(`<iframe width="460" height="315" [src]='${this.safeURL}' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"allowfullscreen></iframe>`)
     // console.log(this.safeHTML)
     // this.postHTML = this.assembleHtml()
